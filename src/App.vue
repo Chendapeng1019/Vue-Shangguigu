29fe46bca7d70f39b60226e4e4b82231126cb271
Chendapeng <714491235@qq.com>
2019-07-09 17:33:20 +0800

    vue第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <!--  给TodoHeader标签对象绑定addItems时间监听   -->
-        <todo-header ref="header" ></todo-header>
-        <todo-list :todos="todos" ></todo-list>
-        <todo-footer :todos="todos" :deleteCompleteTodos="deleteCompleteTodos" :selectAllTodos="selectAllTodos"></todo-footer>
-<!--        <todo-footer>-->
-<!--          <input type="checkbox" v-model="ischeckAll" slot="checkAll" />-->
-<!--          <span slot="count">已完成{{ completeSize }} / 全部{{ todos.length}}</span>-->
-<!--          <button class="btn btn-danger" v-show="completeSize" @click="deleteCompleteTodos" slot="delete">清除已完成任务</button>-->
-<!--        </todo-footer>-->
-      </div>
-    </div>
+   <div>
+     <div v-if="!repoUrl">loading</div>
+     <div v-else>most star repo is <a :href="repoUrl">{{repoName}}</a> </div>
+   </div>
 </template>
-<!--
-绑定事件监听  ---- 订阅消息
-触发事件 ---- 发布消息
--->
 
 <script>
 
@@ -27,6 +14,8 @@ export default {
   },
   data() {
     return {
+      repoUrl: '',
+      repoName: ''
     }
   },
   methods: {
@@ -35,6 +24,8 @@ export default {
   watch: { // 监视
     },
   mounted() { //执行异步代码
+              //发送ajax请求获取数据
+    
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)  //内部会给vm对象和组件对象添加一个属性：$http
 
 /* eslint-disable no-new */
 new Vue({
